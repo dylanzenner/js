dca393f471a1e64d25b40297f28ffecb0b3647a8
Dylan Zenner <dzenner@1board.io>
2023-06-27 19:56:54 -0500

    more js practice

---
+++ basics-02-added-scripts-imports/assets/scripts/app.js
@@ -0,0 +1,6 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+currentResult += 10;
+
+let calculationDescription = `${'heheheheh'}(` + currentResult + "0 + 10) * 3 / 2 - 1";
+outputResult(currentResult, calculationDescription);
---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,11 @@
+let someNumber = 22;
+let result;
+
+result = 18 + someNumber
+
+result -= 10
+result *= 100
+result /= 10
+
+alert(someNumber)
+alert(result)
